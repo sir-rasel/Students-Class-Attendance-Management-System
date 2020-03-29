cd69d68d331644c359862780630b8782ca3bfb85
Saiful Islam Rasel <raselcse97@gmail.com>
2020-03-29 21:32:46 +0600

    Create Basic template of home page

---
+++ home.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="css\button.css">
+</head>
+<body>
+    <h3>Welcome to our "Student's Class Attendance Management System". Thanks for using our service.</h3>
+    <a href="#">LOG IN / SIGN IN</a>
+    <br/><br/>
+    <a href="#">REGISTER / SIGN UP</a>
+</body>
+</html>
